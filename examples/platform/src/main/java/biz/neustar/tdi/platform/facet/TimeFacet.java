7e6a512a109bc2e526934597a6e97173cb47dd06
J. Ian Lindsay <josh.lindsay@gmail.com>
2017-10-17 15:07:53 -0700

    Update TimeFacet.java
    Javascript has spoiled me rotten.

--- examples/platform/src/main/java/biz/neustar/tdi/platform/facet/TimeFacet.java
+++ examples/platform/src/main/java/biz/neustar/tdi/platform/facet/TimeFacet.java
@@ -83,11 +83,13 @@ public class TimeFacet implements TdiPlatformTimeShape {
   public Long timestamp(String timeDateStr) {
     if (!StringUtils.isEmpty(timeDateStr)) {
       Instant instant = Instant.parse(timeDateStr);
-      Long timeInSec = Math.floor(instant.toEpochMilli() / 1000).longValue();
+      Double floor = Math.floor(instant.toEpochMilli() / 1000);
+      Long timeInSec = floor.longValue();
       return timeInSec;
     } else {
       // As we don't have time stamp string, return current epoch time
-      return ((Math.floor(Instant.now().toEpochMilli() / 1000)).longValue());
+      Double floor = Math.floor(Instant.now().toEpochMilli() / 1000);
+      return floor.longValue();
     }
   }
 
