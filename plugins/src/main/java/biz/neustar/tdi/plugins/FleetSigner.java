65a1ace80696249e41b9c77295339397bbfcd3af
jspark311 <josh.lindsay@gmail.com>
2017-11-03 13:18:53 -0700

    Got a grip on the lambdas. Translation from TypeScript twists 180-deg going forward.

--- plugins/src/main/java/biz/neustar/tdi/plugins/FleetSigner.java
+++ plugins/src/main/java/biz/neustar/tdi/plugins/FleetSigner.java
@@ -16,10 +16,12 @@
 
 package biz.neustar.tdi.plugins;
 
+import biz.neustar.tdi.fw.implementation.TdiFlowArguments;
 import biz.neustar.tdi.fw.implementation.TdiImplementationShape;
 import biz.neustar.tdi.fw.keystructure.TdiKeyFlagsEnum;
 import biz.neustar.tdi.fw.keystructure.TdiKeyStructure;
 import biz.neustar.tdi.fw.keystructure.TdiKeyStructureShape;
+import biz.neustar.tdi.fw.canonicalmessage.TdiCanonicalMessageShape;
 import biz.neustar.tdi.fw.platform.TdiPlatformShape;
 import biz.neustar.tdi.fw.platform.facet.crypto.TdiPlatformCryptoShape;
 import biz.neustar.tdi.fw.platform.facet.data.TdiPlatformDataShape;
@@ -50,10 +52,57 @@ public class FleetSigner extends TdiPluginBase {
    */
   private String baseURI = "";
 
+  /**
+   * Generates a new signature around a payload with the fleet key
+   * @param payload Payload to be encased in claims
+   */
+  public Function<Object, CompletableFuture<Object>> fleetSign = null;
+  /**
+   * Appends a fleet signature to the provided JWS
+   * @param jws The JWS to add a signature to
+   */
+  public Function<Object, CompletableFuture<Object>> fleetCosign = null;
+  /**
+   * Verify a server or device signature
+   * @param jws The JWS to be verified
+   */
+  public Function<Object, CompletableFuture<Object>> fleetVerify = null;
+  /**
+   * Execute a full round-trip signing ceremony.  Returns a JWS ready for device consumption
+   * @param payload
+   */
+  public Function<Object, CompletableFuture<Object>> fleetToDevice = null;
+  /**
+   * Execute a full round-trip verify ceremony.  Returns a payload if all verifications pass
+   * @param JWS
+   */
+  public Function<Object, CompletableFuture<Object>> fleetFromDevice = null;
+  /**
+   * Signs a payload as a token
+   * @param payload Payload to be sent
+   */
+  public Function<Object, CompletableFuture<Object>> signToken = null;
+
 
   public FleetSigner(TdiImplementationShape impl, TdiSdkWrapperShape sdkWrapper) {
     super("FleetSigner", impl, sdkWrapper);
     LOG.trace("FleetSigner:constructor()");
+    // Now that we have refs to implementation and SDK, build up our API methods...
+    // NOTE: Since these functions mutually reference, this setup order might be
+    //   important. TODO: Determine this.
+    this.fleetSign = this.impl.buildApiFlow(this.buildFlowFleetSign(), null);
+    this.signToken = this.impl.buildApiFlow(this.buildFlowSignToken(), null);
+    this.fleetCosign = this.impl.buildApiFlow(this.buildFlowFleetCosign(), null);
+    this.fleetVerify = this.impl.buildApiFlow(this.buildFlowFleetVerify(), null);
+    this.fleetToDevice = this.impl.buildApiFlow(this.buildFlowFleetToDev(), null);
+    this.fleetFromDevice = this.impl.buildApiFlow(this.buildFlowFleetFromDev(), null);
+
+//    this.fleetSign = this.impl.buildApiFlow(
+//      this.sdkWrapper.defApi["sign"].flow, // Flow
+//      this.sdkWrapper.defApi["sign"].flowFn(this.imp), // Default
+//      // Override function below (same as a mod)
+//      {
+//      });
   }
 
 
@@ -87,11 +136,233 @@ public class FleetSigner extends TdiPluginBase {
   }
 
 
-  public void serverSign() {
-    LOG.trace("FleetSigner:serverSign()");
+  //public void serverSign() {
+  //  LOG.trace("FleetSigner:serverSign()");
+  //}
+
+  //public void serverVerify() {
+  //  LOG.trace("FleetSigner:serverVerify()");
+  //}
+
+
+
+  private TdiFlowArguments buildFlowFleetSign() {
+    TdiFlowArguments flow = new TdiFlowArguments();
+      //{
+      //  // NOTE: If we want to cause the fleet server's SELF key to sign
+      //  //   messages, we would remove 'setSigners' from the array below.
+      //  _or: ['setSigners'],
+      //  setSigners: (msg: TdiCanonicalMessageShape) => {
+      //    return this.imp.pf.keys
+      //      .getKeyByRole(TdiKeyStructureFlags.ROLE_F_S, msg.currentProject)
+      //      .then(k_fs => {
+      //        let success = false;
+      //        if (k_fs && k_fs.canSign) {
+      //          msg.signers.push(k_fs);
+      //          success = true;
+      //        }
+      //        return success
+      //          ? Promise.resolve(msg)
+      //          : Promise.reject(
+      //              'setSigners() failed to find a signing key for either F_C or F_S.'
+      //            );
+      //      });
+      //  }
+    return flow;
   }
 
-  public void serverVerify() {
-    LOG.trace("FleetSigner:serverVerify()");
+  private TdiFlowArguments buildFlowFleetCosign() {
+    TdiFlowArguments flow = new TdiFlowArguments();
+      //{
+      //  _or: ['setSigners'],
+      //  setSigners: (msg: TdiCanonicalMessageShape) => {
+      //    let success = false;
+      //    return this.imp.pf.keys
+      //      .getKeyByRole(TdiKeyStructureFlags.ROLE_F_S, msg.currentProject)
+      //      .then(k_fs => {
+      //        if (k_fs && k_fs.canSign) {
+      //          msg.signers.push(k_fs);
+      //          success = true;
+      //        } else {
+      //        }
+      //        return success
+      //          ? Promise.resolve(msg)
+      //          : Promise.reject(
+      //              'setSigners() failed to find a signing key for either F_C or F_S.'
+      //            );
+      //      });
+      //  }
+      //}
+    return flow;
+  }
+
+  private TdiFlowArguments buildFlowFleetVerify() {
+    TdiFlowArguments flow = new TdiFlowArguments();
+      //{
+      //  // Prevent the default behavior from taking place for these phases.
+      //  _or: ['prepSignatures', 'handleReturn'],
+      //  prepSignatures: (msg: TdiCanonicalMessageShape) => {
+      //    // get kid values for signatures (prune signatures and check project keystore scope?)
+      //    let verify_count = 0;
+      //    const promise_queue = [];
+      //    for (const i of msg.signaturesToVerify) {
+      //      const t_kid: string = i['parsedHeader']['kid'];
+      //      promise_queue.push(
+      //        this.imp.pf.keys
+      //          .getKey(t_kid)
+      //          .then((t_key: TdiKeyStructure) => {
+      //            // We have knowledge of the key.
+      //            if (t_key.fleet !== '') {
+      //              msg.currentProject = t_key.fleet;
+      //            }
+      //            if (t_key.invalid) {
+      //              msg.verifiers = []; // No point to verifying.
+      //              return Promise.reject(
+      //                `prepSignatures(): Key ${t_key.kid} was marked invalid.`
+      //              );
+      //            }
+      //            msg.verifiers.push(t_key);
+      //            verify_count++;
+      //          })
+      //          .catch((err: string) => {
+      //            return false;
+      //          })
+      //      );
+      //    }
+
+      //    return Promise.all(promise_queue).then(() => {
+      //      if (msg.currentProject === '') {
+      //        this.log('fleetVerify(): WARNING: No currentProject set');
+      //      }
+      //      if (verify_count !== 0) {
+      //        // NOTE: We are not checking for roles here.
+      //        return Promise.resolve(msg);
+      //      } else {
+      //        return Promise.reject(
+      //          'prepSignatures(): No known keys to verify against.'
+      //        );
+      //      }
+      //    });
+      //  },
+      //  handleReturn: (msg: TdiCanonicalMessageShape) => {
+      //    return msg;
+      //  }
+      //}
+    return flow;
+  }
+
+  private TdiFlowArguments buildFlowFleetToDev() {
+    TdiFlowArguments flow = new TdiFlowArguments();
+      //{
+      //  sign: (dataIn: any) => {
+      //    return this.currentApi.sign(dataIn);
+      //  },
+      //  sendToCosigner: (jwsPayload: string) => {
+      //    return this.imp.pf.keys.getSelf().then((self: TdiKeyStructure) => {
+      //      const fleet = self.fleet;
+      //      const kid = self.kid;
+      //      return rp({
+      //        method: 'POST',
+      //        uri: `${this.baseURI}/projects/${fleet}/cosign_for_server/${kid}`,
+      //        body: jwsPayload,
+      //        headers: {
+      //          'content-type': 'application/JOSE+JSON',
+      //          Accept: 'application/JOSE+JSON',
+      //          encoding: 'utf8'
+      //          // Bearer: 'jwt '
+      //        }
+      //      });
+      //    });
+      //  },
+      //  validateCosigner: (requestBody: string) => {
+      //    if (requestBody) {
+      //      return this.fleetVerify(
+      //        requestBody
+      //      ).then((vCanon: TdiCanonicalMessageShape) => {
+      //        return requestBody;
+      //      });
+      //    } else {
+      //      return Promise.reject('No response body in cosigner reply');
+      //    }
+      //  },
+      //  fleetSign: (verifiedJWS: TdiCanonicalMessageShape) => {
+      //    return this.fleetCosign(verifiedJWS);
+      //  }
+      //}
+    return flow;
+  }
+
+  private TdiFlowArguments buildFlowFleetFromDev() {
+    TdiFlowArguments flow = new TdiFlowArguments();
+      //{
+      //  // sign: (dataIn: any) => {
+      //  //   return this.currentApi.sign(dataIn)
+      //  // },
+      //  sendToCosigner: (jwsPayload: string) => {
+      //    return this.imp.pf.keys.getSelf().then((self: TdiKeyStructure) => {
+      //      const fleet = self.fleet;
+      //      const kid = JSON.parse(
+      //        this.imp.pf.util.b64URLDecode(JSON.parse(jwsPayload).payload)
+      //      ).iss;
+      //      return rp({
+      //        method: 'POST',
+      //        uri: `${this
+      //          .baseURI}/projects/${fleet}/cosign_for_edge_device/${kid}`,
+      //        body: jwsPayload,
+      //        // json: true,
+      //        headers: {
+      //          'content-type': 'application/JOSE+JSON',
+      //          Accept: 'application/JOSE+JSON',
+      //          encoding: 'utf8'
+      //          // Bearer: 'jwt '
+      //        }
+      //      });
+      //    });
+      //  },
+      //  validateCosigner: (requestBody: string) => {
+      //    if (requestBody) {
+      //      return this.fleetVerify(
+      //        requestBody
+      //      ).then((vCanon: TdiCanonicalMessageShape) => {
+      //        return requestBody;
+      //      });
+      //    } else {
+      //      return Promise.reject(
+      //        new Error('No response body in cosigner reply')
+      //      );
+      //    }
+      //  },
+      //  fleetSign: (msg: TdiCanonicalMessageShape) => {
+      //    return this.fleetCosign(msg);
+      //  }
+      //}
+    return flow;
+  }
+
+  private TdiFlowArguments buildFlowSignToken() {
+    TdiFlowArguments flow = new TdiFlowArguments();
+      //{
+      //  // NOTE: If we want to cause the fleet server's SELF key to sign
+      //  //   messages, we would remove 'setSigners' from the array below.
+      //  _or: ['setSigners'],
+      //  setSigners: (msg: TdiCanonicalMessageShape) => {
+      //    msg.signatureType = 'compact';
+      //    return this.imp.pf.keys
+      //      .getKeyByRole(TdiKeyStructureFlags.ROLE_EXTERN, msg.currentProject)
+      //      .then(k_fs => {
+      //        let success = false;
+      //        if (k_fs && k_fs.canSign) {
+      //          msg.signers.push(k_fs);
+      //          success = true;
+      //        }
+      //        return success
+      //          ? Promise.resolve(msg)
+      //          : Promise.reject(
+      //              'setSigners() failed to find a signing key for either F_C or F_S.'
+      //            );
+      //      });
+      //  }
+      //}
+    return flow;
   }
 }
