0d272a35f8ee1b0a9f356c6f7cf77096b2c1747d
Yaima Valdivia <yaimamvaldivia@gmail.com>
2017-11-16 15:56:39 -0800

    Add additional flows to JUnit test

--- plugins/src/test/java/biz/neustar/tdi/plugins/FleetSignerTest.java
+++ plugins/src/test/java/biz/neustar/tdi/plugins/FleetSignerTest.java
@@ -25,6 +25,14 @@ public class FleetSignerTest {
 	}
 
 	@Test
+	public void tesFleetSign() throws ExecutionException, InterruptedException {
+		String signedMsg = serverSdk.sign("testMessage");
+		final String logMsg = String.format("Sign message: [%s]: ", signedMsg);
+		log.info(logMsg);
+		assertNotNull(signedMsg);
+	}
+
+	@Test
 	public void testFleetCosign() throws ExecutionException, InterruptedException {
 		String cosignedMsg = serverSdk.cosign(serverSdk.sign("testMessage"));
 		final String logMsg = String.format("Cosign message: [%s]: ", cosignedMsg);
@@ -33,11 +41,29 @@ public class FleetSignerTest {
 	}
 
 	@Test
-	public void testSignToken() throws ExecutionException, InterruptedException {
-		String signedMsg = serverSdk.sign("testMessage");
-		final String logMsg = String.format("Sign message: [%s]: ", signedMsg);
+	public void testFleetVerify() throws ExecutionException, InterruptedException {
+		String fleetToDeviceMsg = serverSdk.fleetToDevice(serverSdk.sign("testMessage"));
+		String verifiedMsg = deviceSdk.verify(fleetToDeviceMsg);
+		final String logMsg = String.format("Verify message: [%s]: ", verifiedMsg);
 		log.info(logMsg);
-		assertNotNull(signedMsg);
+		assertNotNull(verifiedMsg);
+	}
+
+	@Test
+	public void testFleetToDevice() throws ExecutionException, InterruptedException {
+		String fleetToDeviceMsg = serverSdk.fleetToDevice(serverSdk.sign("testMessage"));
+		final String logMsg = String.format("Fleet to device message: [%s]: ", fleetToDeviceMsg);
+		log.info(logMsg);
+		assertNotNull(fleetToDeviceMsg);
+	}
+
+	@Test
+	public void testFleetFromDevice() throws ExecutionException, InterruptedException {
+		String deviceMsg = deviceSdk.sign("fromDeviceMsg");
+		String fleetFromDeviceMsg = serverSdk.fleetFromDevice(deviceMsg);
+		final String logMsg = String.format("Device to fleet msg: [%s]: ", fleetFromDeviceMsg);
+		log.info(logMsg);
+		assertNotNull(fleetFromDeviceMsg);
 	}
 
 }
