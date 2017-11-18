bb8d2bab28695a0ebfcae841905aedcab3285c32
Carl Dunham <cdunham@gmail.com>
2017-11-18 00:37:05 -0800

    tests(plugins): rename helper to avoid conflict
    This package won't be able to use the new `NTDI` class(es) due to circular dependencies.

--- plugins/src/test/java/biz/neustar/tdi/plugins/NTDIHelper.java
+++ plugins/src/test/java/biz/neustar/tdi/plugins/NTDIHelper.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2017 Neustar, Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 package biz.neustar.tdi.plugins;
@@ -32,14 +32,12 @@ import java.util.concurrent.ExecutionException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Helper {
-}
 
-class NTDI {
+class NTDIHelper {
 	private TdiSdkWrapperShape sdkWrapper;
 
-	public NTDI(List<TdiPluginBaseFactory> plugins, String configPath) throws ExecutionException, InterruptedException {
-		sdkWrapper = (new NTDIFactory()).setup(plugins, configPath).get();
+	public NTDIHelper(List<TdiPluginBaseFactory> plugins, String configPath) throws ExecutionException, InterruptedException {
+		sdkWrapper = (new NTDIHelperFactory()).setup(plugins, configPath).get();
 	}
 
 	public String sign(String data) throws ExecutionException, InterruptedException {
@@ -64,7 +62,7 @@ class NTDI {
 	}
 }
 
-class NTDIFactory {
+class NTDIHelperFactory {
 	private Map<String, Object> getConfig(String configPath) {
 		Map<String, Object> map = null;
 		InputStream inStream = getClass().getClassLoader().getResourceAsStream(configPath);
