c42add386713bb6a91261649c81ef4e6e2223d33
Carl Dunham <cdunham@gmail.com>
2017-11-18 00:38:48 -0800

    feat(tdi): Add top-level classes

---
+++ tdi/src/main/java/biz/neustar/tdi/NTDIFactory.java
@@ -0,0 +1,32 @@
+package biz.neustar.tdi;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import biz.neustar.tdi.platform.Platform;
+import biz.neustar.tdi.fw.plugin.TdiPluginBaseFactory;
+import biz.neustar.tdi.fw.wrapper.TdiSdkWrapperShape;
+import biz.neustar.tdi.sdk.TdiSdk;
+import biz.neustar.tdi.sdk.TdiSdkOptions;
+
+import biz.neustar.tdi.Config;
+import biz.neustar.tdi.NTDI;
+
+class NTDIFactory {
+    protected static Logger log = LoggerFactory.getLogger(NTDI.class);
+
+    // ASYNC INIT
+    public CompletableFuture<TdiSdkWrapperShape> setup(List<TdiPluginBaseFactory> plugins, Config config) {
+        TdiSdkOptions sdkOptions = new TdiSdkOptions();
+        sdkOptions.platform = Platform::new;
+        sdkOptions.plugins = plugins;
+        if (config != null) sdkOptions.config = config.map;
+
+        log.debug("config={}", sdkOptions.config);
+
+        return (new TdiSdk(sdkOptions)).init();
+    }
+}
