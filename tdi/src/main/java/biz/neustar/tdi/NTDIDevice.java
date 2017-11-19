4514aac56202221da316aa4eb3f0eec2a0447881
Carl Dunham <cdunham@gmail.com>
2017-11-19 08:33:03 -0800

    refactor: split NTDI into BaseNTDI and NTDIDevice

---
+++ tdi/src/main/java/biz/neustar/tdi/NTDIDevice.java
@@ -0,0 +1,26 @@
+package biz.neustar.tdi;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class NTDIDevice extends BaseNTDI {
+    private static Logger log = LoggerFactory.getLogger(NTDIDevice.class);
+
+    public NTDIDevice() throws ExecutionException, InterruptedException, IOException {
+        super();
+    }
+
+    public String cosign(String msg) throws ExecutionException, InterruptedException {
+        log.debug("cosigning {}", msg);
+        return ((TdiCanonicalMessageShape) this.sdk.api("CosignFlow").apply(msg).get()).getBuiltMessage();
+    }
+
+    public String verify(String msg) throws ExecutionException, InterruptedException {
+        log.debug("verifying {}", msg);
+        return ((String) this.sdk.api("VerifyFlow").apply(msg).get());
+    }
+}
--- tdi/src/main/java/biz/neustar/tdi/NTDIFactory.java
+++ tdi/src/main/java/biz/neustar/tdi/NTDIFactory.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import biz.neustar.tdi.fw.platform.TdiPlatformShapeFactory;
 import biz.neustar.tdi.platform.Platform;
 import biz.neustar.tdi.fw.plugin.TdiPluginBaseFactory;
 import biz.neustar.tdi.fw.wrapper.TdiSdkWrapperShape;
@@ -17,9 +18,18 @@ class NTDIFactory {
     private static Logger log = LoggerFactory.getLogger(NTDIFactory.class);
 
     // ASYNC INIT
+    public CompletableFuture<TdiSdkWrapperShape> setup(Config config) {
+        return setup(null, null, config);
+    }
+
     public CompletableFuture<TdiSdkWrapperShape> setup(List<TdiPluginBaseFactory> plugins, Config config) {
+        return setup(null, plugins, config);
+    }
+
+    public CompletableFuture<TdiSdkWrapperShape> setup(TdiPlatformShapeFactory platform, List<TdiPluginBaseFactory> plugins, Config config) {
+        if (platform == null) platform = Platform::new;
         TdiSdkOptions sdkOptions = new TdiSdkOptions();
-        sdkOptions.platform = Platform::new;
+        sdkOptions.platform = platform;
         sdkOptions.plugins = plugins;
         if (config != null) sdkOptions.config = config.map;
 
