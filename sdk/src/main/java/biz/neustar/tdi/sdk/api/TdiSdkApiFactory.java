c6304a99c68ce4314a1447e72b77b5cca5c240f0
jspark311 <josh.lindsay@gmail.com>
2017-11-20 14:38:00 -0800

    More enums dropped. Change should be self-contained. Flow not longer conflatable with enum order.

--- sdk/src/main/java/biz/neustar/tdi/sdk/api/TdiSdkApiFactory.java
+++ sdk/src/main/java/biz/neustar/tdi/sdk/api/TdiSdkApiFactory.java
@@ -30,7 +30,6 @@ import java.util.Map;
  *
  */
 public class TdiSdkApiFactory {
-
   /**
    * Returns the api flow arguments in the desired manner.
    *
@@ -40,14 +39,11 @@ public class TdiSdkApiFactory {
    * @return map with 'key' as Api name and 'value' as {@link TdiFlowArguments}
    */
   public static Map<String, TdiFlowArguments> buildArgs(TdiImplementationShape imp) {
-
     Map<String, TdiFlowArguments> flows = new HashMap<>();
-
     addSignArgs(imp, flows);
     addCoSignArgs(imp, flows);
     addVerifyArgs(imp, flows);
     addVerifyGeneralArgs(imp, flows);
-
     return flows;
   }
 
@@ -60,20 +56,28 @@ public class TdiSdkApiFactory {
    * @param flows
    *          map to set, key as Api name and value {@link TdiFlowArguments}
    */
-  private static void addSignArgs(TdiImplementationShape imp, Map<String, TdiFlowArguments> flows) {
-
+  private static void addSignArgs(
+      TdiImplementationShape imp,
+      Map<String, TdiFlowArguments> flows) {
     Sign signFlow = new Sign(imp);
     TdiFlowArguments signArgs = new TdiFlowArguments();
 
-    signArgs.addMethod(ApiFlows.SignFlow.handleInit, signFlow::handleInit);
-    signArgs.addMethod(ApiFlows.SignFlow.parseRaw, signFlow::parseRaw);
-    signArgs.addMethod(ApiFlows.SignFlow.setClaims, signFlow::setClaims);
-    signArgs.addMethod(ApiFlows.SignFlow.packClaims, signFlow::packClaims);
-    signArgs.addMethod(ApiFlows.SignFlow.setSigners, signFlow::setSigners);
-    signArgs.addMethod(ApiFlows.SignFlow.sign, signFlow::sign);
-    signArgs.addMethod(ApiFlows.SignFlow.handleReturn, signFlow::handleReturn);
+    /** Create Canonical message and populate payload claim. */
+    signArgs.addMethod(Constants.FlowMethods.handleInit, signFlow::handleInit);
+    /** Optional secondary parsing step for inbound messages. */
+    signArgs.addMethod(Constants.FlowMethods.Signing.parseRaw, signFlow::parseRaw);
+    /** Loads values in to canonical "claims" key value pairs. */
+    signArgs.addMethod(Constants.FlowMethods.Signing.setClaims, signFlow::setClaims);
+    /** Populates canonical "rawPayload" field with serialized claims. */
+    signArgs.addMethod(Constants.FlowMethods.Signing.packClaims, signFlow::packClaims);
+    /** Assign "kid" strings to canonical "signers" array. */
+    signArgs.addMethod(Constants.FlowMethods.Signing.setSigners, signFlow::setSigners);
+    /** Signs the "rawPayload" with the keys from "signers". */
+    signArgs.addMethod(Constants.FlowMethods.Signing.sign, signFlow::sign);
+    /** Handles the canonical "builtMessage" field for final return. */
+    signArgs.addMethod(Constants.FlowMethods.handleReturn, signFlow::handleReturn);
 
-    flows.put(Constants.Api.SignFlow.name(), signArgs);
+    flows.put(Constants.Api.SignFlow, signArgs);
   }
 
   /**
@@ -85,18 +89,22 @@ public class TdiSdkApiFactory {
    * @param flows
    *          map to set, key as Api name and value {@link TdiFlowArguments}
    */
-  private static void addCoSignArgs(TdiImplementationShape imp,
+  private static void addCoSignArgs(
+      TdiImplementationShape imp,
       Map<String, TdiFlowArguments> flows) {
-
     CoSign cosignFlow = new CoSign(imp);
     TdiFlowArguments cosignArgs = new TdiFlowArguments();
 
-    cosignArgs.addMethod(ApiFlows.CosignFlow.handleInit, cosignFlow::handleInit);
-    cosignArgs.addMethod(ApiFlows.CosignFlow.setSigners, cosignFlow::setSigners);
-    cosignArgs.addMethod(ApiFlows.CosignFlow.sign, cosignFlow::sign);
-    cosignArgs.addMethod(ApiFlows.CosignFlow.handleReturn, cosignFlow::handleReturn);
+    /** Create Canonical message &amp; populate payload claim and signatures. */
+    cosignArgs.addMethod(Constants.FlowMethods.handleInit, cosignFlow::handleInit);
+    /** assign "kid" strings to canonical "signers" array. */
+    cosignArgs.addMethod(Constants.FlowMethods.Signing.setSigners, cosignFlow::setSigners);
+    /** Co-signs the message. */
+    cosignArgs.addMethod(Constants.FlowMethods.Signing.sign, cosignFlow::sign);
+    /** Update the built message with heldSignatures. */
+    cosignArgs.addMethod(Constants.FlowMethods.handleReturn, cosignFlow::handleReturn);
 
-    flows.put(Constants.Api.CosignFlow.name(), cosignArgs);
+    flows.put(Constants.Api.CosignFlow, cosignArgs);
   }
 
   /**
@@ -108,22 +116,30 @@ public class TdiSdkApiFactory {
    * @param flows
    *          map to set, key as Api name and value {@link TdiFlowArguments}
    */
-  private static void addVerifyArgs(TdiImplementationShape imp,
+  private static void addVerifyArgs(
+      TdiImplementationShape imp,
       Map<String, TdiFlowArguments> flows) {
-
     Verify verifyFlow = new Verify(imp);
     TdiFlowArguments verifyArgs = new TdiFlowArguments();
 
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.handleInit, verifyFlow::handleInit);
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.unpackEnvelope, verifyFlow::unpackEnvelope);
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.unpackClaims, verifyFlow::unpackClaims);
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.validateClaims, verifyFlow::validateClaims);
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.prepSignatures, verifyFlow::prepSignatures);
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.verifySignatures, verifyFlow::verifySignatures);
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.afterVerify, verifyFlow::afterVerify);
-    verifyArgs.addMethod(ApiFlows.VerifyFlow.handleReturn, verifyFlow::handleReturn);
+    /** Create Canonical message and populate the "receivedMessage" field. */
+    verifyArgs.addMethod(Constants.FlowMethods.handleInit, verifyFlow::handleInit);
+    /** Break "receivedMessage" into its components (header/payload/sigs). */
+    verifyArgs.addMethod(Constants.FlowMethods.Verifying.unpackEnvelope, verifyFlow::unpackEnvelope);
+    /** Parse "rawClaims" in to "claims" key/values. */
+    verifyArgs.addMethod(Constants.FlowMethods.Verifying.unpackClaims, verifyFlow::unpackClaims);
+    /** Validate all "claims" properties. */
+    verifyArgs.addMethod(Constants.FlowMethods.Verifying.validateClaims, verifyFlow::validateClaims);
+    /** Manipulate and/or validate "signaturesToVerify". */
+    verifyArgs.addMethod(Constants.FlowMethods.Verifying.prepSignatures, verifyFlow::prepSignatures);
+    /** Validate signatures to verify relative to "rawPayload". */
+    verifyArgs.addMethod(Constants.FlowMethods.Verifying.verifySignatures, verifyFlow::verifySignatures);
+    /** Run any necessary cleanup or post-validation steps. */
+    verifyArgs.addMethod(Constants.FlowMethods.Verifying.afterVerify, verifyFlow::afterVerify);
+    /** Returns the authenticated message. */
+    verifyArgs.addMethod(Constants.FlowMethods.handleReturn, verifyFlow::handleReturn);
 
-    flows.put(Constants.Api.VerifyFlow.name(), verifyArgs);
+    flows.put(Constants.Api.VerifyFlow, verifyArgs);
   }
 
   /**
@@ -135,22 +151,28 @@ public class TdiSdkApiFactory {
    * @param flows
    *          map to set, key as Api name and value {@link TdiFlowArguments}
    */
-  private static void addVerifyGeneralArgs(TdiImplementationShape imp,
-                                    Map<String, TdiFlowArguments> flows) {
-
+  private static void addVerifyGeneralArgs(
+      TdiImplementationShape imp,
+      Map<String, TdiFlowArguments> flows) {
     VerifyGeneral verifyGeneralFlow = new VerifyGeneral(imp);
     TdiFlowArguments verifyGeneralArgs = new TdiFlowArguments();
+    /** Create Canonical message and populate the "receivedMessage" field. */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.handleInit, verifyGeneralFlow::handleInit);
+    /** Break "receivedMessage" into its components (header/payload/sigs). */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.Verifying.unpackEnvelope, verifyGeneralFlow::unpackEnvelope);
+    /** Parse "rawClaims" in to "claims" key/values. */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.Verifying.unpackClaims, verifyGeneralFlow::unpackClaims);
+    /** Validate all "claims" properties. */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.Verifying.validateClaims, verifyGeneralFlow::validateClaims);
+    /** Manipulate and/or validate "signaturesToVerify". */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.Verifying.prepSignatures, verifyGeneralFlow::prepSignatures);
+    /** Validate signatures to verify relative to "rawPayload". */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.Verifying.verifySignatures, verifyGeneralFlow::verifySignatures);
+    /** Run any necessary cleanup or post-validation steps. */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.Verifying.afterVerify, verifyGeneralFlow::afterVerify);
+    /** Returns the authenticated message. */
+    verifyGeneralArgs.addMethod(Constants.FlowMethods.handleReturn, verifyGeneralFlow::handleReturn);
 
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.handleInit, verifyGeneralFlow::handleInit);
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.unpackEnvelope, verifyGeneralFlow::unpackEnvelope);
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.unpackClaims, verifyGeneralFlow::unpackClaims);
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.validateClaims, verifyGeneralFlow::validateClaims);
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.prepSignatures, verifyGeneralFlow::prepSignatures);
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.verifySignatures, verifyGeneralFlow::verifySignatures);
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.afterVerify, verifyGeneralFlow::afterVerify);
-    verifyGeneralArgs.addMethod(ApiFlows.VerifyGeneralFlow.handleReturn, verifyGeneralFlow::handleReturn);
-
-    flows.put(Constants.Api.VerifyGeneralFlow.name(), verifyGeneralArgs);
+    flows.put(Constants.Api.VerifyGeneralFlow, verifyGeneralArgs);
   }
-
 }
